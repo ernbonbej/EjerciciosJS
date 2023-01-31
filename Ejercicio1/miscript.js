9808d980f7327310a608daf192d67ae71256c6d3
ernbonbej <ebonnail@gmail.com>
2023-01-31 19:46:33 +0100

    EjercicioJS

---
+++ Ejercicio1/miscript.js
@@ -0,0 +1,8 @@
+const largoCubo = 23
+const anchoCubo = 16
+const altoCubo = 13
+
+areaCubo = largoCubo*anchoCubo
+volumenCubo = largoCubo*anchoCubo*altoCubo
+
+console.log('Volumen:', volumenCubo,'cm3','\nÁrea:',areaCubo, 'cm2')
