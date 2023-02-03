88ddcd2d4b4106fb7411f99a19898b27e4edc159
ernbonbej <ebonnail@gmail.com>
2023-02-03 22:14:30 +0100

    ejercicio17

--- Ejercicio17/miscript.js
+++ Ejercicio17/miscript.js
@@ -1,5 +1,8 @@
-enlaces = document.getElementsByTagName(a);
+const enlaces = document.getElementsByTagName('a')
+
+numEnlaces = enlaces.length
+
+const direccion = enlaces[numEnlaces-2]
 
 
-alert(`Número de enlaces de la página: ${enlaces.length}`)
-
+alert(`- La página tiene ${numEnlaces} enlaces\n- El penúltimo enlace dirige a: ${direccion}\n- El tercer párrafo tiene X enlaces`)
