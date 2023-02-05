4499a535b95d8b8fcd7e7527817c6a0f198b721b
ernbonbej <ebonnail@gmail.com>
2023-02-05 18:53:22 +0100

    ejercicio17

--- Ejercicio17/miscript.js
+++ Ejercicio17/miscript.js
@@ -1,8 +1,15 @@
-const enlaces = document.getElementsByTagName('a')
+const enlaces = document.getElementsByTagName('a');
 
-numEnlaces = enlaces.length
+numEnlaces = enlaces.length;
 
-const direccion = enlaces[numEnlaces-2]
+const direccion = enlaces[numEnlaces-2];
 
+const parrafo3 = document.getElementById('third-paragraph');
 
-alert(`- La página tiene ${numEnlaces} enlaces\n- El penúltimo enlace dirige a: ${direccion}\n- El tercer párrafo tiene X enlaces`)
+numEnl = parrafo3.getElementsByTagName('a').length;
+
+alert(`- La página tiene ${numEnlaces} enlaces\n- El penúltimo enlace dirige a: ${direccion}\n- El tercer párrafo tiene ${numEnl} enlaces`);
+
+const nuevoParrafo = document.createElement("p");
+nuevoParrafo.innerHTML = `La página tiene ${numEnlaces} enlaces, el penúltimo enlace dirige a: ${direccion} y el tercer párrafo tiene ${numEnl} enlaces`;
+document.body.appendChild(nuevoParrafo);
